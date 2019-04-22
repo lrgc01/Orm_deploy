cbabcff1a9bba254dfa5e5041b5881662e0f6c01
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-04-22 02:42:14 +0000

    lrc.sytes.net is noy in production certificate. Using DB as docker container.

--- site.sh
+++ site.sh
@@ -77,11 +77,12 @@ export ANSIBLE_SSH_ARGS="-C -o ControlMaster=auto -o ControlPersist=60s -F ${SSH
 # First, full, playbook running - no skip
 SKIP_TAGS=""
 # After first install, the tag "bootstrap_python" can be safely skipped:
-#SKIP_TAGS="--skip-tags \"bootstrap_python\""
+#SKIP_TAGS="--skip-tags bootstrap_python"
 
+# Order is important, since Docker will create de DBMS server
+ansible-playbook -i dockersrv --extra-vars "gather_y_n=yes update_cache_y_n=yes basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS Dockers.yml 
 ansible-playbook -i hasrv --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS HAservers.yml 
 ansible-playbook -i dbsrv --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS DBservers.yml 
-ansible-playbook -i dockersrv --extra-vars "gather_y_n=yes update_cache_y_n=yes basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS Dockers.yml 
 
 # garbage
 rm -f *.retry
